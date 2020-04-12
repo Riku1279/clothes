0120d601237fe1497674d2ff879528e3fb3a91d7
rick <riku@gmail.com>
2020-04-12 08:44:54 +0000

    complete2

--- app/models/coordinate.rb
+++ app/models/coordinate.rb
@@ -4,6 +4,6 @@ class Coordinate < ApplicationRecord
   
   validates :content, presence: true, length: { maximum: 255 }
   
-  has_many :favorites
+  has_many :favorites,dependent: :destroy
   has_many :liked, through: :favorites, source: :user
 end
